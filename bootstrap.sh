73cd0349777724bcb83b8d5f3fa42319e1f7d623
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-03-11 21:19:13 -0500

    add cabal

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update &&
-    sudo apt-get install -y ghc &&
+    sudo apt-get install -y cabal-install &&
     sudo apt-get autoclean -y &&
     sudo apt-get clean -y &&
     sudo rm -rf /var/lib/apt/lists/* \
